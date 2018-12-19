c62727efc48f16cb237e5ec1ab8816399c783347
mfraser4 <m_fraser3@u.pacific.edu>
2018-12-18 20:44:26 -0800

    Setup main Python file with preliminary research
    With this commit, a main.py file is created that does nothing but import
    pandas and PyQt5, a possible GUI package to use in presenting .csv data.

---
+++ data_analytics/main.py
@@ -0,0 +1,24 @@
+from pandas import (
+    read_csv
+)
+from plotly.plotly import (
+    plot
+)
+from PyQt5.QWidgets import (
+    QWidget,
+    QScrollArea,
+    QTableWidget,
+    QVBoxLayout,
+    QTableWidgetItem
+)
+
+
+"""
+Helpful Links:
+    https://plot.ly/pandas/
+    https://pythonspot.com/pyqt5
+"""
+
+# Internal Macros
+__MULTIPLE_SCATTER = 'pandas/multiple-scatter'
+__SIMPLE_HISTOGRAM = 'pandas/simple-histogram'
